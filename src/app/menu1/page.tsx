67dfdb5c1f6bc5bf1fc4c05c530ad244cd68cf5b
rakib38324 <aminul.rakib2022@gmail.com>
2024-09-24 08:12:39 +0600

    complete landing page

--- src/app/menu1/page.tsx
+++ src/app/menu1/page.tsx
@@ -18,18 +18,18 @@ export default function Menu1() {
       <Testimonials />
 
       {/* create apointment banner */}
-      <div className="max-w-screen-xl mx-auto bg-gradient-to-br from-[#07ADF1] via-[#7BCFD9] to-[#B4E0CD] dark:bg-gradient-to-br dark:from-[#357A38] dark:via-[#226E5D] dark:to-[#00579B] lg:flex justify-between py-7 px-5 my-10 md:px-10 md:my-20 rounded-2xl">
+      <div className="max-w-screen-xl mx-auto bg-gradient-to-br from-[#07ADF1] via-[#7BCFD9] to-[#B4E0CD] dark:bg-gradient-to-br dark:from-[#357A38] dark:via-[#226E5D] dark:to-[#00579B] lg:flex justify-between items-center py-7 px-5 my-10 md:px-10 md:my-20 rounded-2xl">
         <div className="my-auto">
           <p className="text-3xl md:text-5xl font-bold dark:text-textDark">
-            Clinic Facilies
+            Ready to get started ?
           </p>
 
           <p className="  text-xl font-normal  dark:text-gray-300 mt-2 md:mt-5">
-            Nam sollicitud nunc, cursus eros vulputate sed. Vestibulum lobortis.
+            Pellentesque ac bibendum tortor. Nulla eget lobortis lacus.
           </p>
         </div>
 
-        <div className="my-auto mt-5 lg:mt-0">
+        <div className="my-auto mt-5">
           <PrimaryButton
             text="CREATE APPOINTMENT NOW!"
             bgColor="bg-white"
@@ -38,7 +38,6 @@ export default function Menu1() {
           />
         </div>
       </div>
-
 
       <OurClinics />
     </div>
